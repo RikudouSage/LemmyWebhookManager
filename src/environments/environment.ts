abff82ac7a57534167d11787388df43fe47d2ebc
Dominik Chrastecky <dominik@chrastecky.cz>
2025-03-20 17:31:56 +0100

    Increase version

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   appTitle: 'Lemmy Webhooks',
-  appVersion: '1.2.4',
+  appVersion: '1.3.1',
   apiUrl: typeof (<any>window)['API_URL'] !== 'undefined' ? (<any>window)['API_URL'] : 'https://api.webhooks.lemmings.world',
 };
